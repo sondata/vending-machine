0983117b567414ac7de1050e51fddf58da36e9e9
Alex Pavlov <a.pavlov@ubiquedigital.com>
2024-06-04 22:38:27 +0300

    Initial boilerplate Next app

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const nextConfig = {
+    sassOptions: {
+      includePaths: [path.join(__dirname, 'styles')],
+    },
+};
+
+export default nextConfig;
